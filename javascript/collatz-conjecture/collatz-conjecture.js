a7861fbd1ddf29e552bbb944954f198d15695c17
Lyndsey Browning <lbrowning86@gmail.com>
2020-08-30 19:23:47 +0100

    add solution to collatz conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -2,7 +2,25 @@
 // This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const step = (number, counter = 1) => {
+    const isEven = number % 2 === 0;
+    const nextNumber = isEven ? number / 2 : number * 3 + 1;
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+    if (nextNumber === 1) {
+        return counter;
+    }
+
+    return step(nextNumber, counter + 1);
+};
+
+export const steps = (n) => {
+    if (Math.sign(n) < 1) {
+        throw new Error("Only positive numbers are allowed");
+    }
+
+    if (n === 1) {
+        return 0;
+    }
+
+    return step(n);
 };
