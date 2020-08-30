b60fe6c5b001b7de609196d8241c47938abd39ab
Lyndsey Browning <lbrowning86@gmail.com>
2020-08-30 19:45:45 +0100

    Refactor collatz conjecture solution

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -2,25 +2,19 @@
 // This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-const step = (number, counter = 1) => {
+const step = (number, counter = 0) => {
+    if (number === 1) return counter;
+
     const isEven = number % 2 === 0;
     const nextNumber = isEven ? number / 2 : number * 3 + 1;
-
-    if (nextNumber === 1) {
-        return counter;
-    }
 
     return step(nextNumber, counter + 1);
 };
 
-export const steps = (n) => {
-    if (Math.sign(n) < 1) {
+export const steps = (number) => {
+    if (number <= 0) {
         throw new Error("Only positive numbers are allowed");
     }
 
-    if (n === 1) {
-        return 0;
-    }
-
-    return step(n);
+    return step(number);
 };
