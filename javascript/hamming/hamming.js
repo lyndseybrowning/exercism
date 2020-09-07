90bf784449aacc5d27c2174f9afce7604d2b546f
Lyndsey Browning <lbrowning86@gmail.com>
2020-09-07 19:31:30 +0100

    refactor hamming solution

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,13 +1,14 @@
-const STRANDS = ["left", "right"];
+const STRAND_POSITIONS = ["left", "right"];
 const ERROR_MESSAGES = {
     empty: "strand must not be empty",
     invalidLength: "left and right strands must be of equal length",
 };
 
-export const compute = (...strands) => {
+export const compute = (strand1, strand2) => {
+    const strands = [strand1, strand2];
     const hasAllEmptyStrands = strands.every((strand) => strand === "");
-    const firstEmptyStrand = STRANDS.find((_, i) => strands[i] === "");
-    const hasInvalidLength = strands[0].length !== strands[1].length;
+    const firstEmptyStrand = STRAND_POSITIONS.find((_, i) => strands[i] === "");
+    const hasInvalidLength = strand1.length !== strand2.length;
 
     if (hasAllEmptyStrands) {
         return 0;
@@ -21,12 +22,5 @@ export const compute = (...strands) => {
         throw new Error(ERROR_MESSAGES.invalidLength);
     }
 
-    const [strand1, strand2] = strands;
-
-    return [...strand1].reduce((distance, piece, index) => {
-        if (piece !== strand2[index]) {
-            distance += 1;
-        }
-        return distance;
-    }, 0);
+    return [...strand1].filter((strand, i) => strand !== strand2[i]).length;
 };
